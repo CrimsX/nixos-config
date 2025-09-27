f665acd9d75274297333b243dc9806ef40b5a7b2
Jordan <jlai.github@gmail.com>
2025-09-26 22:07:16 -0600

    flake cleanup

--- flake.nix
+++ flake.nix
@@ -7,19 +7,19 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "nixpkgs/nixos-25.05";
 
-    nix-flatpak.url = "github:gmodena/nix-flatpak";
+    nix-flatpak = {
+      url = "github:gmodena/nix-flatpak";
+    };
 
-    niri.url = "github:YaLTeR/niri";
-    niri-stable.url = "github:YaLTeR/niri/v25.08"
+    niri = {
+      url = "github:YaLTeR/niri";
+      flake = false;
+    };
 
-    xwayland-satellite.url = "github:Supreeeme/xwayland-satellite";
-    xwayland-satellite-stable.url = "github:Supreeeme/xwayland-satellite/v0.7";
-
-    niri.flake = false;
-    niri-stable.flake = false;
-
-    xwayland-satellite.flake = false;
-    xwayland-satellite-stable.flake = false;
+    xwayland-satellite = {
+      url = "github:Supreeeme/xwayland-satellite";
+      flake = false;
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -48,71 +48,66 @@
   };
 
   outputs = {
-    self,
     nixpkgs,
     home-manager,
-    stylix,
     ...
     } @ inputs:
     let
-      # inherit (self) outputs;
-      inherit (nixpkgs) lib;
-      supportedSystems = [
-        "x86_64-linux"
-      ];
-      forAllSystems = f: lib.genAttrs supportedSystems (system: f system);
-      # forAllSystems = nixpkgs.libgenAttrs systems;
-      # user = "";
-      # hostname = "";
-    in
-    {
+    system = "x86_64-linux";
+    user = "crimsx";
 
-    formatter = forAllSystems (system: nixpkgs.legacyPackages.$(system}.crimsx);
-
-    nixosModules = {
-      waybar = import ./nixos/modules/waybar.nix;
-      default = { ... }: {
-        imports = [
-	  home-manager.nixosModules.ome-manager
-	  self.nixosModules.
-	];
+    pkgs = import nixpkgs {
+      inherit system;
+      config = {
+        allowUnfree = true;
       };
     };
-
-    {
-    /*
+    in {
       nixosConfigurations = {
-        test = nixpkgs.lib.nixosSystem {
-	  system = "x86_64-linux";
-	  specialArgs = { inherit inputs; };
+        L7490 = nixpkgs.lib.nixosSystem {
 	  modules = [
-	    ./hosts/workstation
+	    ./hosts/L7490
+	    stylix.nixosModules.stylix
+	    ./configuration.nix
+	    #./hardware-configuration.nix
+	    ({ pkgs, ... }: {
+	      programs.nvim.defaultEditor = true;
+	    })
+	    home-manager.nixosModules.home-manager {
+	      home-manager = {
+	        useGlobalPkgs = true;
+	        useUserPackages = true;
+	        users.crimsx = import ./home.nix;
+	        backupFileExtension = "backup";
+	      };
+	      /*
+	      home-manager.users.crimsx = {
+	        programs.fastfetch.enable = true;
+	      };
+	      */
+	    }
 	    stylix.nixosModules.stylix
 	  ];
+	  specialArgs = {
+	    inherit inputs system user;
+	    hostname = "L7490";
+	  };
+	};
+	forAllSystems = f: lib.genAttrs supportedSystems (system: f system);
+	# forAllSystems = nixpkgs.libgenAttrs systems;
+	in {
+	  formatter = forAllSystems (system: nixpkgs.legacyPackages.$(system}.crimsx);
+	  nixosModules = {
+	    waybar = import ./nixos/modules/waybar.nix;
+	    default = { ... }: {
+	      imports = [
+	        home-manager.nixosModules.ome-manager
+		self.nixosModules.
+	      ];
+	    };
+	  };
 	};
       };
-      */
-      nixosConfigurations.L7490 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          # Import the previous configuration.nix we used,
-	  # so the old configuration file still takes effect
-	  ./configuration.nix
-	  
-	  home-manager.nixosModules.home-manager
-	  {
-	    home-manager = {
-	      useGlobalPkgs = true;
-	      useUserPackages = true;
-	      users.crimsx = import ./home.nix;
-	      backupFileExtension = "backup";
-	    };
-	    # home-manager.users.crimsx = {
-	    #   programs.fastfetch.enable = true;
-	    # };
-	  }
-	  stylix.nixosModules.stylix
-        ];
-      };
     };
-}
+  };
+};
